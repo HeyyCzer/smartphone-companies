c71f39d69322ce43286bf5ac9fd4001d4d1bcaeb
HeyyCzer <43618377+HeyyCzer@users.noreply.github.com>
2023-03-27 23:08:35 -0300

    🩹 (navigation): Improved backspace navigation

--- srcUi/src/functions.js
+++ srcUi/src/functions.js
@@ -8,6 +8,8 @@ export default function FunctionsComponent() {
 	useEffect(() => {
 		const onKeyDown = ({ key }) => {
 			if (key === 'Backspace' || key === 'Escape') {
+				if (document.querySelectorAll("input:focus").length > 0 || document.querySelectorAll("textarea:focus").length > 0) return;
+
 				axios.post("http://smartphone/keydown", { key });
 			}
 		}
